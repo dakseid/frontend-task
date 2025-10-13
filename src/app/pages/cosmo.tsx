03a4d8d3f1a02807f5d0eca4ff37f6009fef16d0
Esaias Akinyemi <esaiasakinyemi@gmail.com>
2025-10-13 02:16:17 +0100

    1

--- src/app/pages/cosmo.tsx
+++ src/app/pages/cosmo.tsx
@@ -5,7 +5,7 @@ export default function Cosmo() {
     const tokens = useWebSocket("ws://127.0.0.1:8080/connect");
 
     return (
-        <div className="p-6">
+        <div className="p-5">
             <h1 className="text-2xl font-bold mb-4">New Solana Tokens Feed</h1>
             <div className="grid grid-cols-1 gap-4">
                 {tokens.map((token, i) => (
